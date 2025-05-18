a4a761620a06cb43bf67609a28fba6cc2ea1df0e
mabinhang2021 <1223841837@qq.com>
2025-05-18 20:56:30 +0800

    finish webhook sync with ngrok and connect the web hook on clerk dashboard fix the issues about parse and store user name from Clerk webhook

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,6 @@
 import { integer, pgTable, uuid,text,timestamp,uniqueIndex } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   clerkId:text("clerk_id").unique().notNull(),
   name: text("name").notNull(),
