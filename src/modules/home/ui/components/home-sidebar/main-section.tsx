2e48c191e815d7c2737e89bb87b454b9b379d6cc
mabinhang2021 <1223841837@qq.com>
2025-05-18 11:31:12 +0800

    finish home-sidebar and home-navbar

---
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { SidebarGroup, SidebarMenu,SidebarGroupContent,SidebarMenuButton,SidebarMenuItem } from "@/components/ui/sidebar"
+import { Flame, FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react"
+import Link from "next/link"
+import { title } from "process"
+
+const items=[
+    {
+        title: "Home",
+        icon: HomeIcon,
+        url: "/"
+    },
+    {
+        title: "Subscriptions",
+        icon: PlaySquareIcon,
+        url: "/feed/subscriptions",
+        auth: true
+    },
+    {
+        title: "Trending",
+        icon: FlameIcon,
+        url: "/feed/trending",
+    },
+]
+
+export const MainSection = () => {
+    return(
+        <SidebarGroup>
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    {items.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton
+                                asChild
+                                tooltip={item.title}
+                                isActive={false} //todo: change to look at current pathname
+                                onClick={() => {}} //todo dosomething on click
+                            >
+                                <Link href={item.url} className="flex items-center gap-4">
+                                    <item.icon />
+                                    <span className="text-sm">{item.title}</span>
+                                
+                                </Link>
+
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                        
+                </SidebarMenu>
+                
+            </SidebarGroupContent>
+        </SidebarGroup>
+    )
+}
---
+++ src/modules/home/ui/components/home-sidebar/personal-section.tsx
@@ -0,0 +1,58 @@
+"use client"
+import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from "lucide-react"
+import { SidebarGroup, SidebarMenu,SidebarGroupContent,SidebarMenuButton,SidebarMenuItem,SidebarGroupLabel} from "@/components/ui/sidebar"
+import { Flame, FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react"
+import Link from "next/link"
+import { title } from "process"
+
+const items=[
+    {
+        title: "History",
+        icon: HistoryIcon,
+        url: "/playlists/history",
+        auth: true
+    },
+    {
+        title: "Liked Videos",
+        icon: ThumbsUpIcon,
+        url: "/playlists/liked",
+        auth: true
+    },
+    {
+        title: "All Playlists",
+        icon: ListVideoIcon,
+        url: "/playlists",
+        auth: true
+    },
+]
+
+export const PersonalSection = () => {
+    return(
+        <SidebarGroup>
+            <SidebarGroupLabel>You</SidebarGroupLabel>   
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    {items.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton
+                                asChild
+                                tooltip={item.title}
+                                isActive={false} //todo: change to look at current pathname
+                                onClick={() => {}} //todo dosomething on click
+                            >
+                                <Link href={item.url} className="flex items-center gap-4">
+                                    <item.icon />
+                                    <span className="text-sm">{item.title}</span>
+                                
+                                </Link>
+
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                        
+                </SidebarMenu>
+                
+            </SidebarGroupContent>
+        </SidebarGroup>
+    )
+}
